5859d96b05ca0a6fb2673ac6cb9ada19c0536f74
khorramk <khorramk.kbsk@gmail.com>
2018-08-06 23:32:29 +0100

    checking the google loading or not

--- Map.js
+++ Map.js
@@ -1,6 +1,8 @@
 export class Map extends React.Component {
-    componentDidMount() {
-        this.loadMap();
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.google !== this.props.google) {
+            this.loadMap();
+        }
     }
 
     loadMap() {
